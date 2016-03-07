470c9ec454f4c756e131947fef1e5ebc1932843a
Alexander Wong <admin@alexander-wong.com>
2016-03-06 19:19:10 -0700

    Added cluster prep script

---
+++ cluster_prep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+/usr/local/bin/enableAutoUpdate
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo sysctl vm.swappiness=10
+echo "vm.swappiness = 10" | sudo tee -a /etc/sysctl.conf
+
+printf "\n%s\n%s\n%s\n" "10.2.1.50 mapcore.cloud.cybera.ca" "10.2.3.9 mapnode-1.cloud.cybera.ca" "10.2.2.129 mapnode-2.cloud.cybera.ca" | sudo tee -a /etc/hosts
+
