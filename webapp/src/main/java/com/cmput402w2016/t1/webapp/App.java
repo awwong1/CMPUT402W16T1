cf35b33ab6a044e9c93d6801e9f699e9b12b6b73
Alexander Wong <admin@alexander-wong.com>
2016-03-07 22:11:49 -0700

    Added scaffold webapp files

---
+++ webapp/src/main/java/com/cmput402w2016/t1/webapp/App.java
@@ -0,0 +1,13 @@
+package com.cmput402w2016.t1.webapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
