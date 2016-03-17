33f8f142c53890a1f2a4449398a38c0f7c6bb162
Alexander Wong <admin@alexander-wong.com>
2016-03-16 23:54:45 -0600

    New Empty Project

---
+++ MapCore/src/main/java/com/cmput402w2016/t1/Simulator.java
@@ -0,0 +1,10 @@
+package com.cmput402w2016.t1;
+
+/**
+ * Hello world!
+ */
+public class Simulator {
+    public static void main(String[] args) {
+        System.out.println("Simulator Main Method Placeholder.");
+    }
+}
