5eeceb7f6a29723af47f86a42852be159df3b461
Alexander Wong <admin@alexander-wong.com>
2016-03-16 23:54:45 -0600

    New Empty Project

---
+++ MapCore/src/main/java/com/cmput402w2016/t1/WebApp.java
@@ -0,0 +1,7 @@
+package com.cmput402w2016.t1;
+
+public class WebApp {
+    public static void main(String[] args) {
+        System.out.println("Web Application Main Method Placeholder.");
+    }
+}
--- simulator/src/main/java/com/cmput402w2016/t1/data/DatabaseController.java
+++
@@ -1,8 +0,0 @@
-package com.cmput402w2016.t1.data;
-
-/**
- * Created by kent on 10/03/16.
- */
-public interface DatabaseController {
-    void PostTraffic(Traffic traffic);
-}
