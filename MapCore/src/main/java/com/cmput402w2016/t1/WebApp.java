d6d61f30d983243ac9df5742618cfd9e12b79330
Alexander Wong <admin@alexander-wong.com>
2016-03-25 13:48:51 -0600

    Added cli parser, removed other main files

--- MapCore/src/main/java/com/cmput402w2016/t1/WebApp.java
+++
@@ -1,7 +0,0 @@
-package com.cmput402w2016.t1;
-
-public class WebApp {
-    public static void main(String[] args) {
-        System.out.println("Web Application Main Method Placeholder.");
-    }
-}
